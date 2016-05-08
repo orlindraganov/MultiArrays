a04b20f7b43cbf29fabae952ea78580792312ce6
orlindraganov <orlindraganov@gmail.com>
2016-05-09 00:31:49 +0300

    FillTheMatrix Done

--- FillTheMatrix/FillTheMatrix.cs
+++ FillTheMatrix/FillTheMatrix.cs
@@ -15,9 +15,9 @@ class Program
 
         if (fillType == 'a')
         {
-            for (int x = 0; x < arraySize; x++)
+            for (int y = 0; y < arraySize; y++)
             {
-                for (int y = 0; y < arraySize; y++)
+                for (int x = 0; x < arraySize; x++)
                 {
                     arr[y, x] = number;
                     number++;
@@ -31,25 +31,126 @@ class Program
             {
                 for (int y = 0; y < arraySize; y++)
                 {
-                    arr[y, x] = number;
+                    arr[x, y] = number;
                     number++;
                 }
-                for (int y = arraySize - 1; y >= 0; y--)
+                if (x + 1 < arraySize)
                 {
-                    arr[y, x + 1] = number;
+                    for (int y = arraySize - 1; y >= 0; y--)
+                    {
+                        arr[x + 1, y] = number;
+                        number++;
+                    }
+                }
+            }
+        }
+        else if (fillType == 'c')
+        {
+            for (int i = 0; i < arraySize; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    arr[j, arraySize - i + j - 1] = number;
+
+                    if (i < arraySize - 1)
+                    {
+                        arr[arraySize - 1 - j, i - j] = arraySize * arraySize + 1 - number;
+                    }
                     number++;
                 }
             }
         }
 
-        for (int x = 0; x < arraySize; x++)
+        else if (fillType == 'd')
         {
-            for (int y = 0; y < arraySize; y++)
+            string direction = "down";
+            int xArr = 0;
+            int yArr = 0;
+
+            while (number <= arraySize * arraySize)
+            {
+                if (direction == "down")
+                {
+                    if ((yArr == arraySize - 1) || (arr[xArr, yArr + 1] != 0))
+                    {
+                        direction = "right";
+                    }
+                    else
+                    {
+                        arr[xArr, yArr] = number;
+                        number++;
+                        yArr++;
+                        if (number == arraySize * arraySize)
+                        {
+                            arr[xArr, yArr] = number;
+                            break;
+                        }
+                    }
+                }
+                else if (direction == "right")
+                {
+                    if ((xArr == arraySize - 1) || (arr[xArr + 1, yArr] != 0))
+                    {
+                        direction = "up";
+                    }
+                    else
+                    {
+                        arr[xArr, yArr] = number;
+                        number++;
+                        xArr++;
+                        if (number == arraySize * arraySize)
+                        {
+                            arr[xArr, yArr] = number;
+                            break;
+                        }
+                    }
+                }
+                else if (direction == "up")
+                {
+                    if ((yArr == 0) || (arr[xArr, yArr - 1] != 0))
+                    {
+                        direction = "left";
+                    }
+                    else
+                    {
+                        arr[xArr, yArr] = number;
+                        number++;
+                        yArr--;
+                        if (number == arraySize * arraySize)
+                        {
+                            arr[xArr, yArr] = number;
+                            break;
+                        }
+                    }
+                }
+                else if (direction == "left")
+                {
+                    if ((xArr == 0) || (arr[xArr - 1, yArr] != 0))
+                    {
+                        direction = "down";
+                    }
+                    else
+                    {
+                        arr[xArr, yArr] = number;
+                        number++;
+                        xArr--;
+                        if (number == arraySize * arraySize)
+                        {
+                            arr[xArr, yArr] = number;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int y = 0; y < arraySize; y++)
+        {
+            for (int x = 0; x < arraySize - 1; x++)
             {
                 Console.Write(arr[x, y] + " ");
             }
-            Console.WriteLine();
+            Console.WriteLine(arr[arraySize - 1, y]);
         }
-
     }
 }
---
+++ MaximalSum/MaximalSum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

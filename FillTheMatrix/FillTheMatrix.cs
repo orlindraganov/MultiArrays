43086145efb5af7560a4f56f41d21a1f67632144
orlindraganov <orlindraganov@gmail.com>
2016-05-08 21:57:41 +0300

    Init Commit

---
+++ FillTheMatrix/FillTheMatrix.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        int arraySize = int.Parse(Console.ReadLine());
+        char fillType = Convert.ToChar(Console.ReadLine());
+
+        int number = 1;
+
+        int[,] arr = new int[arraySize, arraySize];
+
+        if (fillType == 'a')
+        {
+            for (int x = 0; x < arraySize; x++)
+            {
+                for (int y = 0; y < arraySize; y++)
+                {
+                    arr[y, x] = number;
+                    number++;
+                }
+            }
+        }
+
+        else if (fillType == 'b')
+        {
+            for (int x = 0; x < arraySize; x += 2)
+            {
+                for (int y = 0; y < arraySize; y++)
+                {
+                    arr[y, x] = number;
+                    number++;
+                }
+                for (int y = arraySize - 1; y >= 0; y--)
+                {
+                    arr[y, x + 1] = number;
+                    number++;
+                }
+            }
+        }
+
+        for (int x = 0; x < arraySize; x++)
+        {
+            for (int y = 0; y < arraySize; y++)
+            {
+                Console.Write(arr[x, y] + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
